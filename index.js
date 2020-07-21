15710fcde05ee2c52e0f189f1bf00e951eb0dea3
Nathan Yordanos Tadesse <natetadesse4991@gmail.com>
2020-07-21 11:26:20 -0400

    added app.get request

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ let apiKey = process.env.APIKEY;
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use(express.static('public'))
-
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {weather: null, error: null});
 })
 
 app.post('/', (req, res) => {
